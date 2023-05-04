19fc9660fe365d3ddd28ac93fc41d5e0d02f0fe0
Matthew Bare <69798023+Animatted@users.noreply.github.com>
2023-05-03 23:13:02 -0600

    User page basic functionality in place

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
@@ -21,41 +22,99 @@ class UserWidget extends StatefulWidget {
 class _UserWidgetState extends State<UserWidget> {
   final user = FirebaseAuth.instance.currentUser;
   final db = FirebaseFirestore.instance;
+  String? emailaddress = '';
+
+  Future<List> populateCards() async {
+    var cards = [];
+    var data = await db.collection(emailaddress.toString()).get();
+    for (var docSnapshot in data.docs) {
+      print('${docSnapshot.id} => ${docSnapshot.data()}');
+      cards.add(Entry.fromFirestore(docSnapshot, null));
+      print(cards);
+    }
+    return cards;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final userlist = db.collection("users").doc(user?.email);
-    userlist.get().then((DocumentSnapshot doc) {
-      final data = doc.data() as Map<String, dynamic>;
-    });
+    emailaddress = user?.email;
+    print(emailaddress);
+    var cardlist = populateCards();
     return Scaffold(
-      body: Column(
-        children: ListView.builder(itemBuilder: ((context, index) {
-          return Container(
-            child: Card(
-              child: InkWell(splashColor: Colors.blue,),
-            ),
-          )
-        })),
-        ),
-      ),
+      body: FutureBuilder(
+          future: cardlist,
+          builder: (BuildContext context, snapshot) {
+            List<Widget> children;
+            if (snapshot.hasData) {
+              children = <Widget>[
+                ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (context, index) {
+                      var currentEntry = snapshot.data?.elementAt(index);
+                      return Card(
+                          child: InkWell(
+                        splashColor: Colors.blue,
+                        onTap: () async {
+                          await Clipboard.setData(
+                              ClipboardData(text: '${currentEntry.pword}'));
+                        },
+                        child: Text('${currentEntry.name}'),
+                      ));
+                    })
+              ];
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Loading...'),
+                ),
+              ];
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: children,
+              ),
+            );
+          }),
     );
   }
 }
 
 class Entry {
-  const Entry(
-    {required this.name,
-    required this.pword}
-  );
+  final String? name;
+  final String? pword;
 
-  factory Entry.fromDB(Map<String, dynamic> data) {
-    final name = data['name'] as String;
-    final pword = data['pword'] as String;
+  Entry({this.name, this.pword});
 
-    return Entry(name: name, pword: pword);
+  factory Entry.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    print(data);
+    return Entry(
+      name: data?['name'],
+      pword: data?['pword'],
+    );
   }
-
-  final String name;
-  final String pword;
 }
